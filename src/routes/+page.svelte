03bd7e924b1332eb25a04aa3ab1d2bc570197d38
SaveKit <kittipit582@gmail.com>
2023-10-18 18:40:26 +0700

    main menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Button from '../components/Button.svelte';
+	import settingImage from '$lib/images/settings.png';
+	import speakerUmage from '$lib/images/speaker-filled-audio-tool.png';
+</script>
+
+<img src={settingImage} alt="settings" width="50px" height="50px" />
+<img src={speakerUmage} alt="settings" width="50px" height="50px" />
+<div>
+	<a href="./game">Start</a>
+	<!-- <a href="./game">Game mode</a> -->
+	<a href="./help">How to play</a>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 16px;
+		/* height: 100vh; */
+		padding-top: 45vh;
+	}
+
+	a {
+		text-decoration: none;
+		color: rgb(255, 0, 0);
+		font-size: 16px;
+		font-weight: 600;
+		font-family: 'Roboto', sans-serif;
+		background-color: black;
+	}
+
+	a:hover {
+		background-color: #15ff00;
+	}
+
+	img {
+		margin-right: 10px;
+	}
+</style>
