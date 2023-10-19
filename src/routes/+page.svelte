8d1a856118e085c8ede7e3c99bfe88a8ef89445d
SaveKit <kittipit582@gmail.com>
2023-10-19 18:28:50 +0700

    main menu by save almost done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,31 @@
 <script>
-	import settingImage from '$lib/images/image-2_0.png';
-	import speakerUmageop from '$lib/images/image-3_0.png';
-	import speakerUmagecs from '$lib/images/image_0.png';
-  
-	let showSpeakerImage = true; 
-	let imageSource = speakerUmageop; 
+	import settingImage from '$lib/images/settings.png';
+	import speakerImageOn from '$lib/images/sound-on.png';
+	import speakerImageOff from '$lib/images/sound-off.png';
+
+	let showSpeakerImage = true;
+	let imageSource = speakerImageOn;
 	function toggleImage() {
-	  showSpeakerImage = !showSpeakerImage;
-	  imageSource = showSpeakerImage ? speakerUmageop : speakerUmagecs;
+		showSpeakerImage = !showSpeakerImage;
+		imageSource = showSpeakerImage ? speakerImageOn : speakerImageOff;
 	}
-  </script>
-  
-  <p>
+</script>
+
+<p>
 	<img src={settingImage} alt="settings" width="50px" height="50px" />
-	<img src={imageSource} alt="settings" width="50px" height="50px" on:click={toggleImage} class="clickable-image" />
-  </p>
-  
-  <div>
+	<button on:click={toggleImage} class="clickable-image">
+		<img src={imageSource} alt="settings" width="50px" height="50px" />
+	</button>
+</p>
+
+<div>
 	<h1>Candy Rotater</h1>
 	<a href="./game">🎃 Start 🦇</a>
 	<a href="./help">How to play🧟‍♂️</a>
-  </div>
-  
-  <style>
-  	@font-face {
+</div>
+
+<style>
+	@font-face {
 		font-family: myFirstFont;
 		src: url('$lib/fonts/MN KRASIP.ttf');
 	}
@@ -38,44 +40,45 @@
 	}
 
 	div {
-	  display: flex;
-	  flex-direction: column;
-	  justify-content: center;
-	  align-items: center;
-	  gap: 24px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 24px;
 	}
-  
+
 	a {
-	  text-decoration: none;
-	  color: #fff;
-	  background: var(--base-black);
-	  font-size: 40px;
-	  font-weight: 600;
-	  font-family: 'Roboto', sans-serif;
-	  padding-left: 24px;
-	  padding-right: 24px;
-	  padding-top: 18px;
-	  padding-bottom: 12px;
-	  border-radius: 980px;
+		text-decoration: none;
+		color: #fff;
+		background: var(--base-black);
+		font-size: 40px;
+		font-weight: 600;
+		font-family: 'Roboto', sans-serif;
+		padding-left: 24px;
+		padding-right: 24px;
+		padding-top: 18px;
+		padding-bottom: 12px;
+		border-radius: 980px;
 	}
-  
+
 	a:hover {
-	  background-color: var(--base-orange);
+		background-color: var(--base-orange);
 	}
-  
+
 	img {
 		margin-top: 0%;
 		margin-bottom: 0%;
 		margin-right: 10px;
 	}
-  
+
 	.clickable-image {
-	  cursor: pointer;
-	  transition: transform 0.3s ease;
+		background: none;
+		border: none;
+		cursor: pointer;
+		transition: transform 0.3s ease;
 	}
-  
+
 	.clickable-image:hover {
-	  transform: scale(1.1);
+		transform: scale(1.1);
 	}
-  </style>
-  
+</style>
