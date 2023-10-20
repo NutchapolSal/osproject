e5715ecfde398df0880274c65f9d77b124601f1a
NutchapolSal <Nutchapol.sal@gmail.com>
2023-10-21 03:09:46 +0700

    target grid

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -14,28 +14,46 @@
 </script>
 
 <h1>epic gamo</h1>
-<div class="spinny" style:rotate={`${0.25 * rotVal}turn`}>
-	{#each playerGrid as row}
-		<div>
-			{#each row as state}
-				<GameSquare bind:state />
+<div class="gamecontainer">
+	<div>
+		<div class="spinny" style:rotate={`${0.25 * rotVal}turn`}>
+			{#each playerGrid as row, y}
+				<div>
+					{#each row as state, x}
+						<GameSquare bind:state corner={x === 0 && y === 0} />
+					{/each}
+				</div>
 			{/each}
 		</div>
-	{/each}
+	</div>
+
+	<div>
+		<div>
+			<p>target</p>
+			<div class="spinny" style:rotate="0 turn">
+				{#each targetGrid as row, y}
+					<div class="rowcontainer">
+						{#each row as state, x}
+							<GameSquare {state} corner={x === 0 && y === 0} noninteractive small />
+						{/each}
+					</div>
+				{/each}
+			</div>
+		</div>
+	</div>
 </div>
 
-{#each playerGrid as row}
-	<div>
-		{#each row as state}
-			{state ? '✅' : '❌'}
-		{/each}
-	</div>
-{/each}
-<p>{lastRandded}</p>
-
 <style>
+	div.gamecontainer {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+	}
 	div.spinny {
 		display: inline-block;
 		transition: rotate 0.5s cubic-bezier(0.33, 1, 0.68, 1);
+	}
+	div.rowcontainer {
+		margin: 0;
 	}
 </style>
--- src/routes/game/GameSquare.svelte
+++ src/routes/game/GameSquare.svelte
@@ -1,17 +1,45 @@
 <script lang="ts">
 	export let state: boolean;
+	export let noninteractive: boolean = false;
+	export let small: boolean = false;
+	export let corner: boolean = false;
 </script>
 
-<button on:click={() => (state = !state)} class:stateOn={state}>
-	{state ? 'y' : 'n'}
-</button>
+{#if !noninteractive}
+	<button
+		class="gamesquare"
+		on:click={() => {
+			if (!noninteractive) state = !state;
+		}}
+		class:stateOn={state}
+		class:corner
+		class:small
+	/>
+{:else}
+	<div class="gamesquare" class:corner class:small class:stateOn={state} />
+{/if}
 
 <style>
-	button {
+	.gamesquare {
 		width: 20vmin;
 		height: 20vmin;
+		border: #000000 1px solid;
 	}
-	button.stateOn {
+	div.gamesquare {
+		display: inline-block;
+	}
+	.gamesquare.small {
+		width: 10vmin;
+		height: 10vmin;
+	}
+	.gamesquare.stateOn {
 		background-color: black;
+		color: white;
+	}
+	.gamesquare.corner {
+		background: linear-gradient(to bottom right, #ffff00 20%, #ffff0000 20%);
+	}
+	.gamesquare.corner.stateOn {
+		background: linear-gradient(to bottom right, #ffff00 20%, #ffff0000 20%), black;
 	}
 </style>
