d70167c65e59f4243d598e672062738cbfcdfc77
NutchapolSal <Nutchapol.sal@gmail.com>
2023-10-27 20:37:05 +0700

    refactor gamesquare css

--- src/routes/game/GameSquare.svelte
+++ src/routes/game/GameSquare.svelte
@@ -17,33 +17,37 @@
 	let buttonThis: HTMLButtonElement;
 </script>
 
-{#if !noninteractive}
-	<button
-		bind:this={buttonThis}
-		class="gamesquare"
-		on:pointermove={(e) => {
-			if (e.buttons === 1) {
-				if (!stophold) {
-					state = holdingpaint;
+<div style:display="flex">
+	{#if !noninteractive}
+		<button
+			bind:this={buttonThis}
+			class="gamesquare"
+			on:pointermove={(e) => {
+				if (e.buttons === 1) {
+					if (!stophold) {
+						state = holdingpaint;
+					}
+				} else {
+					stophold = false;
 				}
-			} else {
+			}}
+			on:pointerdown={(e) => {
+				buttonThis.releasePointerCapture(e.pointerId);
+				holdingpaint = !state;
+				state = holdingpaint;
 				stophold = false;
-			}
-		}}
-		on:pointerdown={(e) => {
-			buttonThis.releasePointerCapture(e.pointerId);
-			holdingpaint = !state;
-			state = holdingpaint;
-			stophold = false;
-		}}
-		class:stateOn={state}
-		class:corner
-		class:small
-		class:blanked
-	/>
-{:else}
-	<div class="gamesquare" class:corner class:small class:stateOn={state} class:blanked />
-{/if}
+			}}
+			class:stateOn={state}
+			class:small
+			class:blanked
+		/>
+	{:else}
+		<div class="gamesquare" class:small class:stateOn={state} class:blanked />
+	{/if}
+	{#if corner}
+		<div class="cornercase" class:small class:blanked />
+	{/if}
+</div>
 
 <style>
 	button {
@@ -51,48 +55,53 @@
 		border: none;
 	}
 
-	.gamesquare {
+	.gamesquare,
+	.cornercase {
 		width: 20vmin;
 		height: 20vmin;
+		border-radius: 2.5vmin;
+	}
+	.gamesquare.small,
+	.cornercase.small {
+		width: 10vmin;
+		height: 10vmin;
+		border-radius: 1.25vmin;
+	}
+
+	.gamesquare {
 		background-image: url('$lib/images/candy.png');
 		background-size: cover;
 		background-repeat: no-repeat;
 		background-position: center;
-		filter: grayscale(100%) drop-shadow(0px 0px 5px var(--base-black));
+		filter: grayscale(100%) brightness(1) drop-shadow(0px 0px 5px var(--base-black));
 		touch-action: none;
-		border-radius: 15px;
+		transition: filter 0.05s cubic-bezier(0.215, 0.61, 0.355, 1);
 	}
 	div.gamesquare {
 		display: inline-block;
 	}
-	.gamesquare.small {
-		width: 10vmin;
-		height: 10vmin;
-	}
 	.gamesquare.stateOn {
-		filter: grayscale(0%) drop-shadow(0px 0px 5px var(--base-black));
+		filter: grayscale(0%) brightness(1) drop-shadow(0px 0px 5px var(--base-black));
 	}
-	.gamesquare.corner {
-		background: linear-gradient(to bottom right, #ffff00 20%, #ffff0000 20%),
-			url('$lib/images/candy-grey.png');
-		filter: grayscale(0%) drop-shadow(0px 0px 5px var(--base-black));
-		background-size: cover;
-		background-repeat: no-repeat;
-		background-position: center;
+	.cornercase {
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		background: linear-gradient(135deg, #ffff00 20%, transparent 20%);
+		mix-blend-mode: overlay;
+		pointer-events: none;
+		transition: width 0.1s cubic-bezier(0.215, 0.61, 0.355, 1),
+			height 0.1s cubic-bezier(0.215, 0.61, 0.355, 1);
 	}
-	.gamesquare.corner.stateOn {
-		background: linear-gradient(to bottom right, #ffff00 20%, #ffff0000 20%),
-			url('$lib/images/candy.png');
-		background-size: cover;
-		background-repeat: no-repeat;
-		background-position: center;
+
+	.gamesquare.blanked {
+		filter: grayscale(100%) brightness(1.8) drop-shadow(0px 0px 5px var(--base-black));
+		transition: filter 0.5s cubic-bezier(0.215, 0.61, 0.355, 1);
 	}
-	.gamesquare.blanked,
-	.gamesquare.blanked.corner {
-		background: url('$lib/images/candy-grey.png');
-		filter: grayscale(0%) brightness(1.8) drop-shadow(0px 0px 5px var(--base-black));
-		background-size: cover;
-		background-repeat: no-repeat;
-		background-position: center;
+	.cornercase.blanked {
+		width: 0px;
+		height: 0px;
+		transition: width 0.5s cubic-bezier(0.215, 0.61, 0.355, 1),
+			height 0.5s cubic-bezier(0.215, 0.61, 0.355, 1);
 	}
 </style>
