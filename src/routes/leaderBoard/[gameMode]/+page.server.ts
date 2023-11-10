ed1e4427ecc8377a7212a6ca8a763c90def432f0
NutchapolSal <Nutchapol.sal@gmail.com>
2023-11-10 15:18:34 +0700

    hide shadowbanned users from leaderboard

--- src/routes/leaderBoard/[gameMode]/+page.server.ts
+++ src/routes/leaderBoard/[gameMode]/+page.server.ts
@@ -3,13 +3,16 @@ import { error } from '@sveltejs/kit';
 import { reverseMapGameModes } from '../../gameModes';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params, url }) => {
+export const load: PageServerLoad = async ({ params, url, locals }) => {
+	const session = await locals.auth.validate();
+	const shadowbanned = session?.user.shadowbanned ?? false;
+
 	const gameMode = reverseMapGameModes(params.gameMode);
 	if (gameMode == null) {
 		throw error(404, 'unknown game mode');
 	}
 	return {
-		scores: await getLeaderboard(gameMode),
+		scores: await getLeaderboard(gameMode, shadowbanned),
 		gameMode: gameMode,
 		url: {
 			href: url.href
