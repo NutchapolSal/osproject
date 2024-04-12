1f5b3cd1bc3dced3210209e737b28a24b17fcdd6
NutchapolSal <nutchapol.sal@gmail.com>
2024-04-12 21:56:02 +0700

    add seed test

--- tests/autogame.ts
+++ tests/autogame.ts
@@ -1,19 +1,34 @@
 import { expect, type Page } from '@playwright/test';
 
-export async function playGame(page: Page, gridsDone: number) {
+export async function playGame(page: Page, gridsDone: number, waitForGameOver = true) {
 	await page.getByRole('link').filter({ hasText: 'Start' }).click();
 	await expect(page.locator('div.square.game.blanked')).toHaveCount(9);
 	await expect(page.locator('div.square.game.blanked')).toHaveCount(0);
 	await expect(page.locator('button.square.game')).toHaveCount(9);
+	const gridStates = [];
 	for (let grids = 0; grids < gridsDone; grids++) {
 		const squareStates = await page
 			.locator('div.square.game')
 			.evaluateAll((squares) => squares.map((square) => square.classList.contains('stateOn')));
+		const playerGridRotate = await page
+			.locator('div.spinny:has(button.square.game)')
+			.evaluate((spinny) => spinny.style.rotate);
+		const targetGridRotate = await page
+			.locator('div.spinny:has(div.square.game)')
+			.evaluate((spinny) => spinny.style.rotate);
+		gridStates.push({
+			squareStates,
+			playerGridSpinny: playerGridRotate,
+			targetGridSpinny: targetGridRotate
+		});
 		for (let i = 0; i < squareStates.length; i++) {
 			if (squareStates[i]) {
 				await page.locator('button.square.game').nth(i).click();
 			}
 		}
 	}
-	await expect(page.getByText('game over')).toBeVisible({ timeout: 120000 });
+	if (waitForGameOver) {
+		await expect(page.getByText('game over')).toBeVisible({ timeout: 120000 });
+	}
+	return gridStates;
 }
