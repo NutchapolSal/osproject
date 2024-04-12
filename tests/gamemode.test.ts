039cec3bf2ccce8331ea6a5fb1e4ebc87895cb60
SaveKit <kittipit582@gmail.com>
2024-04-12 12:14:57 +0700

    gamemode setup test

---
+++ tests/gamemode.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@playwright/test';
+
+test('normal mode can set up', async ({ page }) => {
+    await page.goto('/');
+    await page.getByRole('button', { name: 'normal' }).click();
+    await page.getByRole('link').filter({ hasText: 'Start' }).click();
+    await expect(page.locator('div.square.game.blanked')).toHaveCount(9);
+    await expect(page.locator('div.square.game.blanked')).toHaveCount(0);
+    await page.waitForTimeout(3000);
+    await expect(page.locator('div.square.game.blanked')).toHaveCount(0);
+});
+
+test('memory mode can set up', async ({ page }) => {
+    await page.goto('/');
+    await page.getByRole('button', { name: 'memory' }).click();
+    await page.getByRole('link').filter({ hasText: 'Start' }).click();
+    await expect(page.locator('div.square.game.blanked')).toHaveCount(9);
+    await expect(page.locator('div.square.game.blanked')).toHaveCount(0);
+    await page.waitForTimeout(3000);
+    await expect(page.locator('div.square.game.blanked')).toHaveCount(9);
+});   
