23a5c84d67d8436d667739ae12f4e8c2d4dc0939
NutchapolSal <nutchapol.sal@gmail.com>
2024-04-12 20:27:31 +0700

    add testing for not allowing test apis in production

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,25 +1,47 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run dev',
-		port: 5173
-	},
+	webServer: [
+		{
+			command: 'npm run dev',
+			port: 5173
+		},
+		{
+			command: 'npm run build && npm run preview',
+			port: 4173
+		}
+	],
 	testDir: 'tests',
 	projects: [
 		{
 			name: 'setup db',
-			testMatch: /db\.setup\.ts/
+			testMatch: /db\.setup\.ts/,
+			use: {
+				baseURL: 'http://localhost:5173'
+			}
 		},
 		{
 			name: 'setup userdata',
 			testMatch: /userdata\.setup\.ts/,
-			dependencies: ['setup db']
+			dependencies: ['setup db'],
+			use: {
+				baseURL: 'http://localhost:5173'
+			}
 		},
 		{
 			name: 'test',
 			testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-			dependencies: ['setup userdata']
+			dependencies: ['setup userdata'],
+			use: {
+				baseURL: 'http://localhost:5173'
+			}
+		},
+		{
+			name: 'production test',
+			testMatch: /(.+\.)?prodtest\.[jt]s/,
+			use: {
+				baseURL: 'http://localhost:4173'
+			}
 		}
 	]
 };
