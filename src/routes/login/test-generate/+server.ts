f793571fb483375037b9468c313f267660bc592d
NutchapolSal <nutchapol.sal@gmail.com>
2024-04-12 10:53:36 +0700

    add user key function to login/test-generate

--- src/routes/login/test-generate/+server.ts
+++ src/routes/login/test-generate/+server.ts
@@ -1,14 +1,44 @@
 import { dev } from '$app/environment';
 import { auth } from '$lib/server/lucia';
-import { OAuthRequestError } from '@lucia-auth/oauth';
+import { LuciaError } from 'lucia';
 
-export const GET = async ({ locals }) => {
+export const GET = async ({ url, locals }) => {
 	if (!dev) {
 		return new Response(null, { status: 403 });
 	}
+	const keyString = url.searchParams.get('key');
+	const key =
+		keyString != null
+			? {
+					providerId: 'test',
+					providerUserId: keyString,
+					password: null
+			  }
+			: null;
+	if (key) {
+		try {
+			const existingUser = await auth.useKey(key.providerId, key.providerUserId, key.password);
+			const session = await auth.createSession({
+				userId: existingUser.userId,
+				attributes: {}
+			});
+			locals.auth.setSession(session);
+			return new Response(null, {
+				status: 302,
+				headers: {
+					Location: `/`
+				}
+			});
+		} catch (e) {
+			if (!(e instanceof LuciaError && e.message == 'AUTH_INVALID_KEY_ID')) {
+				console.log(e);
+				return new Response(null, { status: 500 });
+			}
+		}
+	}
 	try {
 		const user = await auth.createUser({
-			key: null,
+			key: key,
 			attributes: { display_name: 'user', shadowbanned: false }
 		});
 
@@ -25,9 +55,6 @@ export const GET = async ({ locals }) => {
 		});
 	} catch (e) {
 		console.log(e);
-		if (e instanceof OAuthRequestError) {
-			return new Response(null, { status: 400 });
-		}
 		return new Response(null, { status: 500 });
 	}
 };
